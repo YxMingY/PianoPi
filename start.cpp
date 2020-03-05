d00e230d404031eaf4ad4259479fe09fd011bc87
YxMingY <526817087@qq.com>
2020-03-05 10:41:23 +0000

    add detector

--- start.cpp
+++ start.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 #include "piano.h"
+#include <wiringPi.h>
+int playstar();
 int main()
 {
-	Piano p(25);
+        int on=24;
+
+        wiringPiSetup();
+        pinMode(on,INPUT);
+        while(1){
+                //printf("%d\n",digitalRead(on));
+                if(digitalRead(on)){
+                        playstar();
+                }
+                delay(10);
+        }
+        return 0;
+}
+int playstar()
+{
+	Piano p(25,250);
 	p.play(do1)
 		.play(do1)
 		.play(so1)
