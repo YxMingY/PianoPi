a03867c7b180ba66f496b5d24ee52285f74b53ae
YxMingY <526817087@qq.com>
2020-03-05 07:50:27 +0000

    rewrite note generating ways

--- piano.cpp
+++ piano.cpp
@@ -11,17 +11,21 @@ void bell(int pin,int freq,int time=500)
 {
 	int i,delay_t;
 	digitalWrite(pin,LOW);
+	//A half note's microsecond time
 	time = (int)(1000000*time/1000.0/2);
-	delay_t = time/freq;
-	freq *= 2/3;
+	//Each half waves' microsecond time 
+	delay_t = 1000000/freq/2;
+	//How many waves in total??
+	freq = time/(double)delay_t;
+	//Ring 3/4 time and sleep 1/4 time to make the note distinct
+	freq *= 3/4.0;
 	for(i=0;i<freq;i++) {
 		digitalWrite(pin,HIGH);
-		//cout<<(pin)<<endl;
 		delayMicroseconds(delay_t);
 		digitalWrite(pin,LOW);
 		delayMicroseconds(delay_t);
 	}
-	delayMicroseconds(time/3);
+	delayMicroseconds(time/4);
 }
 Piano::Piano(int pin,int ms):m_pin(pin),m_time(ms)
 {
